6ed4d63d9331e957897bb2a83bfdbfe911b6a796
Mario Gemoll <mariogemoll@gmx.net>
2023-05-20 17:39:34 +0100

    Clean up interface of byteshares functions

--- byteshares.py
+++ byteshares.py
@@ -1,20 +1,27 @@
 import galois
 import numpy as np
+from typing import Tuple
 
-def create_shares(field, k: int, n: int, secret):
-    assert(k < n)
-    assert(n < field.order - 1)
+GF = galois.GF(2**8)
+
+Share = Tuple[int, int]
+
+
+def create_shares(k: int, n: int, secret: int) -> list[Share]:
+    assert (1 <= k <= n < GF.order - 1)
+    assert (0 <= secret <= 255)
     # Select a random polynomial p of degree k-1 with p(0) = secret
-    coefficients = np.append(field.Random(k - 1), secret)
-    p = galois.Poly(coefficients, field=field)
-    assert(p(0) == secret)
+    coefficients = np.append(GF.Random(k - 1), secret)
+    p = galois.Poly(coefficients, field=GF)
+    assert (p(0) == secret)
     # The secret shares are the evaluations of the polynomial at points 1, 2, ...n
     return [(i, int(p(i))) for i in range(1, n + 1)]
 
-def reconstruct_secret(field, shares):
+
+def reconstruct_secret(shares: list[Share]) -> int:
     # Split up the points into lists of x and y coordinates and convert them into field elements
-    xs, ys = map(field, zip(*shares))
+    xs, ys = map(GF, zip(*shares))
     # Reconstruct the polynomial using Lagrange interpolation
     p = galois.lagrange_poly(xs, ys)
     # The secret is the evaluation of the reconstructed polynomial at x=0
-    return p(0)
+    return int(p(0))
--- run.py
+++ run.py
@@ -1,21 +1,19 @@
 from byteshares import create_shares, reconstruct_secret
-import galois
 import random
 
 def test():
-    GF = galois.GF(2 ** 8)
-    secret = GF.Random()
+    secret = random.randint(0, 255)
     print(f'Secret: {secret}')
     
     k = 3
     n = 5
-    shares = create_shares(GF, 3, 5, secret)
+    shares = create_shares(3, 5, secret)
     
     for (i, share) in shares:
         print(f'Share {i}: {share}')
     
     subset = random.sample(shares, 3)
-    reconstructed_secret = reconstruct_secret(GF, subset)
+    reconstructed_secret = reconstruct_secret(subset)
     assert(reconstructed_secret == secret)
     print(f'Reconstructed secret: {reconstructed_secret}')
 
